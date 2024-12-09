fc828a78250cf214abb7715aff6af1cf0e586bb2
CJ Pye <jimmycliff4@gmail.com>
2024-12-08 18:52:17 -0600

    uhhh stuff

---
+++ CatchAll/CapturesList.swift
@@ -0,0 +1,18 @@
+//
+//  CapturesList.swift
+//  CatchAll
+//
+//  Created by CJ Pye on 10/25/24.
+//
+
+import SwiftUI
+
+struct CapturesList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CapturesList()
+}
---
+++ CatchAll/Views/CatchAllButton.swift
@@ -0,0 +1,18 @@
+//
+//  CatchAllButton.swift
+//  CatchAll
+//
+//  Created by CJ Pye on 10/26/24.
+//
+
+import SwiftUI
+
+struct CatchAllButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CatchAllButton()
+}
